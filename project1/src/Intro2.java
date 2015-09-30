fa07560c77cafc78af4639ad4ead0f883f52f93c
karenrojas1994 <arojas@atl.yearup.org>
2015-09-30 01:45:46 -0700

    new proj

---
+++ project1/src/Intro2.java
@@ -0,0 +1,12 @@
+
+public class Intro2 {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+System.out.println("Ana Rojas");
+System.out.println("My favorate book is New Moon");
+System.out.println("My son is three years old");
+	}
+
+}
