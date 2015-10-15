7c4ba68716450f361e64ca4af0f5e95d9190769e
karenrojas1994 <arojas@atl.yearup.org>
2015-10-15 09:46:43 -0700

    dayOfWeek

---
+++ DayOfWeek/src/DayOfWeek.java
@@ -0,0 +1,48 @@
+
+import java.util.Scanner;
+
+public class DayOfWeek {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Enter a new day of week");
+		Scanner sc = new Scanner(System.in);
+		String dayOfWeek = sc.nextLine();
+		EvaluateDayOfWeek(dayOfWeek);
+	}
+public static void EvaluateDayOfWeek(String dayOfWeek)
+{
+if (dayOfWeek != null)
+{
+if (!dayOfWeek.isEmpty())	
+{
+	if(dayOfWeek.equalsIgnoreCase("Monday") ||
+		dayOfWeek.equalsIgnoreCase("Tueday") ||
+		dayOfWeek.equalsIgnoreCase("Wednesday") ||
+		dayOfWeek.equalsIgnoreCase("Thursday") ||
+		dayOfWeek.equalsIgnoreCase("Friday"))
+		{
+		System.out.println(dayOfWeek + " is a weekday ");
+		}
+	else if (dayOfWeek.equalsIgnoreCase("Saturday")||
+			dayOfWeek.equalsIgnoreCase("Sunday"))
+	{
+	System.out.println(dayOfWeek + " is a weekend day! ");
+		}
+else
+{    
+	System.out.println("DayOfWeek is an empty string!!");
+}
+}
+
+else
+{
+	System.out.println("Day of week is an empty string!!");
+}
+}
+else
+{
+	System.out.println("You have given me a null string!!");
+	}
+}
+}
